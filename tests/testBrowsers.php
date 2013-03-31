3f95a870c2c3ab682a74fd32eb6c6ac884e58fb0
Sergey Nehaenko <sergey.nekhaenko@gmail.com>
2013-03-31 10:49:34 +0300

    update to version 1.1.5

--- tests/testBrowsers.php
+++ tests/testBrowsers.php
@@ -2,7 +2,7 @@
 /**
  *  test case.
  */
-class test extends PHPUnit_Framework_TestCase {
+class testBrowsers extends PHPUnit_Framework_TestCase {
 	
 	/**
 	 * Prepares the environment before running a test.
@@ -33,6 +33,39 @@ class test extends PHPUnit_Framework_TestCase {
 		$detector = new DetectBrowser("Mozilla/5.0 (SAMSUNG; SAMSUNG-GT-S8500/S8500XXJH3; U; Bada/1.0; en-us) AppleWebKit/533.1 (KHTML, like Gecko) Dolfin/2.0 Mobile WVGA SMM-MMS/1.2.0 OPN-B");
 		$browser = $detector->get_browser();
 		$this->assertEquals('Dolphin', $browser['name']);
+	}
+	
+	/**
+	 * AOL Explorer test
+	 */
+	public function testAOLExplorer()
+	{
+		// Skyfire
+		$detector = new DetectBrowser("Mozilla/4.0 (compatible; MSIE 8.0; AOL 9.0; Windows NT 5.1; Trident/4.0; GTB6.5; BO1IE8_v1;ENUS)");
+		$browser = $detector->get_browser();
+		$this->assertEquals('AOL Explorer', $browser['name']);
+	}
+	
+	/**
+	 * Konqueror test
+	 */
+	public function testKonqueror()
+	{
+		// Skyfire
+		$detector = new DetectBrowser("Mozilla/5.0 (compatible; Konqueror/3.1; X11)");
+		$browser = $detector->get_browser();
+		$this->assertEquals('Konqueror', $browser['name']);
+	}
+	
+	/**
+	 * Netscape Navigator test
+	 */
+	public function testNetscapeNavigator()
+	{
+		// Skyfire
+		$detector = new DetectBrowser("Mozilla/5.0 (Windows; U; Win 9x 4.90; de-DE; rv:0.9.2) Gecko/20010726 Netscape6/6.1");
+		$browser = $detector->get_browser();
+		$this->assertEquals('Netscape Navigator', $browser['name']);
 	}
 	
 	/**
